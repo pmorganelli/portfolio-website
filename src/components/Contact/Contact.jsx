68418f35f689ea575cc78f82e42567eda143a566
Peter Morganelli <peter.morganelli@tufts.edu>
2024-07-12 14:16:05 -0400

    shortened contact links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,15 +15,15 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <Mail />
-          <a href="mailto:petercarlmorganelli@gmail.com">petercarlmorganelli@gmail.com</a>
+          <a href="mailto:petercarlmorganelli@gmail.com">Email</a>
         </li>
         <li className={styles.link}>
           <Linkedin />
-          <a href="https://www.linkedin.com/in/peter-morganelli-102860258/">linkedin.com/petermorganelli</a>
+          <a href="https://www.linkedin.com/in/peter-morganelli-102860258/">LinkedIn</a>
         </li>
         <li className={styles.link}>
           <Github />
-          <a href="https://github.com/pmorganelli">github.com/pmorganelli5</a>
+          <a href="https://github.com/pmorganelli">GitHub</a>
         </li>
       </ul>
     </footer>
